d7605356e7ee498850c823e93400c058a616a8d2
unknown <sancheznunezjuanpabl07@gmail.com>
2024-09-30 20:01:05 -0600

    Movil

---
+++ script.js
@@ -0,0 +1,39 @@
+alert("Alerta");
+//Comentar codigo
+
+/*
+Comentar codigo con diversas lineas
+*/
+
+//18 de septiembre del 2024, me llamo Juan Pablo Sánchez Núñez del grupo 5IV9
+
+//Variables
+/*
+Las variables guardan información
+que se puede reutilizar
+*/
+
+
+//Declaracion de variables
+
+let nombre = "Juan Pablo";
+const PI = 3.1415;
+var edad = 18;
+
+
+/*Tipos de datos
+Cadena de texto  (string): "Hola", 'Mundo'
+Numero  (numbres): 5, 13, 16
+Boleanos (boolean): true, false
+*/
+let mensaje = "Buenas bro.";
+let numero = 42;
+let estudiante = true;
+
+//operadores aritmaticos
+let a=2;
+let b=5;
+let suma = a+b;
+let resta=a-b;
+let multiplicacion=a*b;
+let division=a/b;
